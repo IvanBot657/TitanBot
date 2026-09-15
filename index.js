b298ae03278d9671b4eef100445d1185b6bf35b7
IvanBot657 <ivancaballero991e@gmail.com>
2026-09-15 00:40:41 -0500

    Export musica from index.js

--- index.js
+++ index.js
@@ -1076,7 +1076,9 @@ ${resultadoXP.nivel}
                ejecutado = true;
                }
              }
-
+            
+             module.exports = musica;
+             module.exports.musica = musica;
             
             // =========================================
             // GRUPOS
